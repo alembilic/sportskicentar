03670086878f1d0da72bfc9f0edca15722dcbe4b
Alem Bilic <alembily@gmail.com>
2024-03-01 20:21:36 +0100

    added selection resource

--- database/migrations/2024_02_24_143916_add_club_id_to_tables.php
+++ database/migrations/2024_02_24_143916_add_club_id_to_tables.php
@@ -12,15 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('memberships', function (Blueprint $table) {
-            $table->foreignId('club_id')->constrained();
+            $table->foreignId('club_id')->nullable()->constrained();
         });
 
         Schema::table('players', function (Blueprint $table) {
-            $table->foreignId('club_id')->constrained();
+            $table->foreignId('club_id')->nullable()->constrained();
         });
 
         Schema::table('membership_types', function (Blueprint $table) {
-            $table->foreignId('club_id')->constrained();
+            $table->foreignId('club_id')->nullable()->constrained();
         });
     }
 
