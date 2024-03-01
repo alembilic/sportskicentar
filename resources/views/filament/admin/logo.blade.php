13e3b749dba4da1537ceff02b79af491deeced1f
Alem Bilic <alembily@gmail.com>
2024-03-01 21:07:20 +0100

    added selection resource

--- resources/views/filament/admin/logo.blade.php
+++ resources/views/filament/admin/logo.blade.php
@@ -19,7 +19,7 @@
         <div class="m-auto ml-3 flex text-xl font-bold leading-5 tracking-tight text-gray-950 dark:text-white">
             {{ cache()->get('club.' . auth()->user()->club_id)['name'] }}
         </div>
-    @else
+    @elseif(cache()->get('club.' . 1))
         <img src="{{ asset('storage/' . cache()->get('club.' . 1)['logo']) }}"
              alt="{{ cache()->get('club.' . 1)['name'] }}"
              class="logo-image-login"/>
